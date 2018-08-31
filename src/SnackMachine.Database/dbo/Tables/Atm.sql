3a0c0d77a180404b09d78636cfe0f05a0c225857
Guillaume BADINIER <g.badinier@hotmail.fr>
2018-08-31 17:09:56 +0200

    Bounded Context ATM

---
+++ src/SnackMachine.Database/dbo/Tables/Atm.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Atm] (
+    [AtmID]             INT             IDENTITY (1, 1) NOT NULL,
+    [MoneyCharged]      DECIMAL (18, 2) NOT NULL,
+    [OneCentCount]      INT             NOT NULL,
+    [TenCentCount]      INT             NOT NULL,
+    [QuarterCentCount]  INT             NOT NULL,
+    [OneDollarCount]    INT             NOT NULL,
+    [FiveDollarCount]   INT             NOT NULL,
+    [TwentyDollarCount] INT             NOT NULL,
+    CONSTRAINT [PK_Atm] PRIMARY KEY CLUSTERED ([AtmID] ASC)
+);
+
---
+++ src/SnackMachine.Logic/Atms/Atm.cs
@@ -0,0 +1,60 @@
+﻿using DDDInPractice.Logic.Common;
+using DDDInPractice.Logic.SharedKernel;
+using static DDDInPractice.Logic.SharedKernel.Money;
+
+namespace DDDInPractice.Logic.Atms
+{
+    public class Atm : AggregateRoot
+    {
+        private const decimal ComissionRate = 0.01m;
+        public virtual Money MoneyInside { get; protected set; } = None;
+        public virtual decimal MoneyCharged { get; protected set; }
+
+        public virtual string CanTakeMoney(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                return "Invalid amount";
+            }
+
+            if (MoneyInside.Amount < amount)
+            {
+                return "Not enough money";
+            }
+
+            if (!MoneyInside.CanAllocate(amount))
+            {
+                return "Not enough change";
+            }
+
+            return string.Empty;
+        }
+
+        public virtual void TakeMoney(decimal amount)
+        {
+            var output = MoneyInside.Allocate(amount);
+            MoneyInside -= output;
+
+            var amountWithCommission = CalculateAmountWithComission(amount);
+            MoneyCharged += amountWithCommission;
+        }
+
+        private decimal CalculateAmountWithComission(decimal amount)
+        {
+            var comission = amount * ComissionRate;
+            var lessThanCent = comission % 0.01m;
+
+            if (lessThanCent > 0)
+            {
+                comission = comission - lessThanCent + 0.01m;
+            }
+
+            return amount + comission;
+        }
+
+        public virtual void LoadMoney(Money money)
+        {
+            MoneyInside += money;
+        }
+    }
+}
